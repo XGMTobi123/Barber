eaff3dbe3c2dcb28fa6455bf3f924ff397cb1452
XGMTobi123 <darioterrafan@gmail.com>
2022-10-26 02:09:06 +0500

    a

---
+++ resources/js/elements/Image.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+    .row(:class="classCss" :style="styleCss")
+        slot
+</template>
+
+<script>
+
+    import ComputedElements from '@/js/computed/Elements'
+
+    export default {
+        name: "RowElement",
+        props: ['classCss', 'styleCss'],
+        // computed:
+        //     {
+        //         ...ComputedElements
+        //     }
+    }
+</script>
+
+<style lang="scss">
+</style>
---
+++ resources/js/elements/List.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+    .logo(:class="classCss", :style="styleCss")
+        img(src='@/img/Logo.png' alt="logo").logo__img
+        .logo__text arber
+</template>
+
+<script>
+    import ComputedElements from '@/js/computed/Elements'
+    export default {
+        name: "Logo",
+        props: ['classCss', 'styleCss']
+    }
+</script>
+
+<style>
+</style>
